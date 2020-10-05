b919cacc42720063e855ffa09312edaca6fe91db
RyanLuttrell <ryan.m.luttrell@gmail.com>
2020-10-05 17:31:53 -0600

    Initial commit

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'const uri = "mongodb+srv://ryan12345:R01Luttrell@practice-mern-stack.yemnj.mongodb.net/<dbname>?retryWrites=true&w=majority";'
+    mongoURI: "mongodb+srv://ryan12345:R01Luttrell@practice-mern-stack.yemnj.mongodb.net/test?retryWrites=true&w=majority"
 }
